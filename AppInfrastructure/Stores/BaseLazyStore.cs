e43a2c0a3c9202c58441fa2b53b095ac444b2612
JessJake <wont.miner@gmail.com>
2022-09-02 03:01:16 +0300

    Initial commit

---
+++ AppInfrastructure/Stores/BaseLazyStore.cs
@@ -0,0 +1,26 @@
+﻿namespace AppInfrastructure.Stores;
+
+/// <summary>
+///     Base Lazy Realize for IStore.
+/// </summary>
+/// <typeparam name="TValue">Value with some type</typeparam>
+public class BaseLazyStore<TValue> : IStore<TValue> 
+{
+    #region CurrentValue
+
+    private Lazy<TValue?> _currentValue = new (()=>default);
+    
+    public TValue? CurrentValue
+    {
+        get => _currentValue.Value;
+        set
+        {
+            _currentValue =  new Lazy<TValue?>(() => value);
+            CurrentValueChangedNotifier?.Invoke();
+        }
+    }
+
+    #endregion
+
+    public event Action? CurrentValueChangedNotifier;
+}
