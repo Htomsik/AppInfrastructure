faa2aa234689820a9429cdca9cc548ca126f49d0
JessJake <wont.miner@gmail.com>
2022-09-02 15:01:37 +0300

    Add generic store

---
+++ AppInfrastructure/Stores/DefaultStore/BaseLazyStore.cs
@@ -0,0 +1,66 @@
+﻿namespace AppInfrastructure.Stores.DefaultStore;
+
+/// <summary>
+///     Base Lazy Realize for IStore.
+/// </summary>
+/// <typeparam name="TValue">Value with some type</typeparam>
+public class BaseLazyStore<TValue> : BaseLazyStore, IStore<TValue>
+{
+    #region CurrentValue
+    
+    public new  TValue? CurrentValue
+    {
+        get => (TValue?)_currentValue.Value;
+        set
+        {
+            _currentValue =  new Lazy<object?>(() => value);
+            OnCurrentValueChanged();
+        }
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public BaseLazyStore(TValue value) => CurrentValue = value;
+
+    public BaseLazyStore(){}
+
+    #endregion
+    
+}
+
+public class BaseLazyStore : IStore
+{
+    #region CurrentValue
+
+    protected Lazy<object?> _currentValue = new (()=>default);
+    
+    public object? CurrentValue
+    {
+        get => _currentValue.Value;
+        set
+        {
+            _currentValue =  new Lazy<object?>(() => value);
+            OnCurrentValueChanged();
+        }
+    }
+
+    #endregion
+
+    #region CurrentValueChangedNotifier
+
+    protected void OnCurrentValueChanged() =>  CurrentValueChangedNotifier?.Invoke();
+    public event Action? CurrentValueChangedNotifier;
+
+    #endregion
+
+    #region Constructors
+
+    public BaseLazyStore(object value) => CurrentValue = value;
+
+    public BaseLazyStore(){}
+
+    #endregion
+    
+}
---
+++ AppInfrastructure/Stores/DefaultStore/IStore.cs
@@ -0,0 +1,27 @@
+﻿namespace AppInfrastructure.Stores.DefaultStore;
+
+/// <summary>
+///     Simple generic some value storage
+/// </summary>
+public interface IStore<TValue> : IStore
+{
+   new TValue CurrentValue { get; set; }
+   
+}
+
+/// <summary>
+///     Simple some value storage
+/// </summary>
+/// <typeparam name="TValue">Value with some type</typeparam>
+public interface IStore
+{
+    /// <summary>
+    ///     Current value in store
+    /// </summary>
+     object? CurrentValue { get; set; }
+
+    /// <summary>
+    ///     Notify when value is changed  
+    /// </summary>
+    event Action? CurrentValueChangedNotifier;
+}
