6c6f5cb5f5a95baa8036a61d1752251299464939
JessJake <wont.miner@gmail.com>
2022-09-02 20:55:42 +0300

    add ICloseServices realize

---
+++ AppInfrastructure/Services/StoreServices/BaseLazyStoreCloseService.cs
@@ -0,0 +1,51 @@
+﻿using AppInfrastructure.Services.NavigationServices.Close;
+using AppInfrastructure.Stores.DefaultStore;
+
+namespace AppInfrastructure.Services.StoreServices;
+
+/// <summary>
+///     Base realize for ICloseServices
+/// </summary>
+public class BaseLazyStoreCloseService : ICloseServices
+{
+    #region Fields
+
+    protected readonly Lazy<IStore>  _store;
+
+    #endregion
+
+    #region Methods
+
+    public void Close() => _store.Value.CurrentValue = null;
+
+    #endregion
+
+    #region Constructors
+
+
+    /// <param name="store">Some store</param>
+    /// <exception cref="ArgumentNullException">Then store is null</exception>
+    public BaseLazyStoreCloseService(IStore store) =>
+        _store = store is null 
+            ? throw new ArgumentNullException(nameof(store))
+            : new Lazy<IStore>(()=> store);
+
+    #endregion
+    
+}
+
+/// <summary>
+///     Base generic realize for ICloseServices
+/// </summary>
+public class BaseLazyStoreCloseService<TValue> : BaseLazyStoreCloseService
+{
+    #region Constructors
+
+    /// <param name="store">Some store</param>
+    /// <exception cref="ArgumentNullException">Then store is null</exception>
+    public BaseLazyStoreCloseService(IStore<TValue> store) : base(store)
+    {
+    }
+
+    #endregion
+}
